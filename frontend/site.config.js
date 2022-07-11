1ddc00b8e24fdb2571024de736a90b86773004df
Avanish Pandey <avanish.pandey@un.org>
2022-07-11 22:16:23 +0530

    update structure

---
+++ frontend/site.config.js
@@ -0,0 +1,16 @@
+// this script feeds the SEO component on each page
+
+export default {
+  siteMetadata: {
+    title: "NextJS Landing Page",
+    author: {
+      name: "Test Project",
+      summary:
+        "[your summary here!]",
+    },
+    description:
+      "",
+    siteUrl: "https://[your-website-here].com",
+    language: "en-US",
+  },
+};
